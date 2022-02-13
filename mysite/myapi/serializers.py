81628819b6a21ad047429bb561da31219d8acc97
Sk <shay.kremer@khealth.ai>
2022-02-13 20:18:07 +0200

    change to flask

--- mysite/myapi/serializers.py
+++
@@ -1,9 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-
-from .models import Hero
-
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Hero
-        fields = ('name', 'alias')
